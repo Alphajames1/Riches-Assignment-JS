f2f03d7077796abdbfd4a177c0acb0633dbc82b1
sjamesadeshina <sjamesadeshina@gmail.com>
2023-02-08 15:39:11 +0100

    java second assignment

---
+++ let myDog = {.js
@@ -0,0 +1,21 @@
+let myDog = {
+    name: 'Tobi',
+    age: 2
+}
+let myAge = myDog.name
+myAge
+
+myDog.hight = 1.2;
+console.log('hight', myDog);
+
+
+let myElectronics = ['TV', 'Radio', 'Fridge']
+myElectronics.push('AC')
+myElectronics.unshift('Remote')
+let stringElectronics = myElectronics.toString()
+stringElectronics
+myElectronics.sort()
+myElectronics
+myElectronics.reverse()
+myElectronics
+
---
+++ work.js
@@ -0,0 +1,21 @@
+// let userName = "James"
+// let password = "Pass1234"
+
+// if(userName === "James" && password === "Pass1234"){
+//     console.log('correct', 'Login successful');
+// }else{
+//     console.log('Invalide, Username or password')
+// }
+
+let score = prompt("please enter Your Score")
+
+score = Number(score)
+
+if(score> 69){
+    alert("Your Grade is A")
+}else if(score > 59){
+    alert("Your Grade is B")
+}else if(score > 49){
+    alert("Your Grade is C")
+}
+else if(score > 39)
